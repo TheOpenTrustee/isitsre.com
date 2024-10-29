4dfc20749f1f26d2d9fe874d32f1fc6269b2d885
Daniel J. Holmes (jaitaiwan) <dan@centricwebestate.com>
2024-10-29 22:49:54 +1000

    fix: Ensure builds are subdir namespaced for ghpages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: "/isitsre.com",
     output: 'export',
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
